c71d7da48f166fe12e4631754cbccb1262f5fe48
GanzaMos <moskalik.ganza@gmail.com>
2020-05-22 16:19:32 +0300

    Added thrust sound effect

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -7,11 +7,13 @@ public class Rocket : MonoBehaviour
 {
 
     Rigidbody rigidBody;
+    AudioSource audioSource;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -36,6 +38,17 @@ public class Rocket : MonoBehaviour
         if (Input.GetKey(KeyCode.Space)) //can thrust while rotating
         {
             rigidBody.AddRelativeForce(Vector3.up);
+
+            if (audioSource.isPlaying == false)
+            {
+                audioSource.Play();
+            }
+
         }
+        else if (Input.GetKeyUp(KeyCode.Space))
+        {
+            audioSource.Stop();
+        }
+
     }
 }
